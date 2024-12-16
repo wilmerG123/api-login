9a83ca9c3b047f4a4f1c1f26fb251c6823abb572
wilmerG123 <garnicawilmer@gmail.com>
2024-12-16 14:26:26 -0500

    switch database

--- src/main/java/com/project/DataSourceConfig.java
+++ src/main/java/com/project/DataSourceConfig.java
@@ -9,11 +9,14 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DataSourceConfig {
 
+	/*
 	@Bean
 	public DataSource getDataSource() {
 		return DataSourceBuilder.create().driverClassName("com.mysql.cj.jdbc.Driver")
 				.url("jdbc:mysql://localhost:3306/projectNeww?useSSL=false&serverTimezone=UTC").username("root")
 				.password("00000000").build();
 	}
+	 */
+
 
 }
